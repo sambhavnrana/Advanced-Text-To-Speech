f16e2a92e932b5bcaad9bff6dc668321c26345d5
Sambhav Rana <89438969+sambhavnrana@users.noreply.github.com>
2023-10-29 21:32:21 +0530

    Updated Home Page UI

--- lib/Pages/Auth/homepage.dart
+++ lib/Pages/Auth/homepage.dart
@@ -15,84 +15,95 @@ class _HomePage extends State<HomePage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: ColorSchemes.orangeC,
-          title: const Text("MAIN APP"),
+          title: const Text(
+            "        Welcome to ATTS App",
+            style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w900,
+                color: ColorSchemes.whiteC,
+                textBaseline: TextBaseline.alphabetic),
+          ),
         ),
-        body: SafeArea(
-          child: Column(
-            children: [
-              
-               Image(
-                width: Responsiveness.screenWidth(context) * 0.5, 
-                  fit: BoxFit.cover,
-                  image: const AssetImage('assets/images/welcome.png')),
-              SizedBox(
-                height: Responsiveness.screenHeight(context) * 0.05,
-              ),
-              Column(
-                children: [
-                  const Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(15.0),
-                      child: Text("Welcome to the App ",
-                          style: TextStyle(
-                              fontSize: 32.0,
-                              fontWeight: FontWeight.bold,
-                              color: ColorSchemes.blackC)),
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                Image(
+                    width: Responsiveness.screenWidth(context) * 0.5,
+                    fit: BoxFit.cover,
+                    image: const AssetImage('assets/images/welcome.png')),
+                SizedBox(
+                  height: Responsiveness.screenHeight(context) * 0.05,
+                ),
+                Column(
+                  children: [
+                    const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(15.0),
+                        child: Text("Welcome to the App ",
+                            style: TextStyle(
+                                fontSize: 32.0,
+                                fontWeight: FontWeight.bold,
+                                color: ColorSchemes.blackC)),
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: Responsiveness.screenHeight(context) * 0.03,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                ColorSchemes.orangeC),
-                            fixedSize: MaterialStateProperty.all<Size>(Size(
-                                Responsiveness.screenWidth(context) * 0.85,
-                                Responsiveness.screenHeight(context) * 0.06))),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/textsummarizer');
-                        },
-                        child: const Text("Text Summarizer"),
-                      ),
-                      SizedBox(
-                        height: Responsiveness.screenHeight(context) * 0.03,
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                ColorSchemes.orangeC),
-                            fixedSize: MaterialStateProperty.all<Size>(Size(
-                                Responsiveness.screenWidth(context) * 0.85,
-                                Responsiveness.screenHeight(context) * 0.06))),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/texttospeech');
-                        },
-                        child: const Text("Text to Speech"),
-                      ),
-                      SizedBox(
-                        height: Responsiveness.screenHeight(context) * 0.03,
-                      ),
-                      ElevatedButton(
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                ColorSchemes.orangeC),
-                            fixedSize: MaterialStateProperty.all<Size>(Size(
-                                Responsiveness.screenWidth(context) * 0.85,
-                                Responsiveness.screenHeight(context) * 0.06))),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/speechtotext');
-                        },
-                        child: const Text("Speech to Text"),
-                      ),
-                    ],
-                  )
-                ],
-              )
-            ],
+                    SizedBox(
+                      height: Responsiveness.screenHeight(context) * 0.03,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  ColorSchemes.orangeC),
+                              fixedSize: MaterialStateProperty.all<Size>(Size(
+                                  Responsiveness.screenWidth(context) * 0.85,
+                                  Responsiveness.screenHeight(context) *
+                                      0.06))),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/textsummarizer');
+                          },
+                          child: const Text("Text Summarizer"),
+                        ),
+                        SizedBox(
+                          height: Responsiveness.screenHeight(context) * 0.03,
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  ColorSchemes.orangeC),
+                              fixedSize: MaterialStateProperty.all<Size>(Size(
+                                  Responsiveness.screenWidth(context) * 0.85,
+                                  Responsiveness.screenHeight(context) *
+                                      0.06))),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/texttospeech');
+                          },
+                          child: const Text("Text to Speech"),
+                        ),
+                        SizedBox(
+                          height: Responsiveness.screenHeight(context) * 0.03,
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  ColorSchemes.orangeC),
+                              fixedSize: MaterialStateProperty.all<Size>(Size(
+                                  Responsiveness.screenWidth(context) * 0.85,
+                                  Responsiveness.screenHeight(context) *
+                                      0.06))),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/speechtotext');
+                          },
+                          child: const Text("Speech to Text"),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
